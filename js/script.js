f5e600d869918c86a534398b0f49b7e4b4c00e6b
Akesel <aksel4455@gmail.com>
2022-10-14 20:18:40 +0200

    Made the dropdown responsive

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-
 //Dark mode section
 const darkMode = document.querySelector(".darkmodeButton");
 const textTarget = document.querySelectorAll("h1, h2, p, a");
 const navbar = document.querySelector("#navbar");
 const body = document.querySelector("body");
+const dropdown = document.querySelector("#jumpDiv");
+const dropdownItem = document.querySelectorAll(".jumpLink");
 
 darkMode.addEventListener("click", () => {
     if (darkMode.textContent === "Dark Mode") {
@@ -11,6 +12,7 @@ darkMode.addEventListener("click", () => {
     } else if (darkMode.textContent === "Light Mode") {
         darkMode.textContent = "Dark Mode";
     }
+    dropdown.classList.toggle("darkmodeBackground");
     navbar.classList.toggle("darkmodeBackground");
     body.classList.toggle("darkmodeBackground");
     textTarget.forEach(e => e.classList.toggle("darkmodeText"));
@@ -18,9 +20,7 @@ darkMode.addEventListener("click", () => {
 
 //Jump dropdown section
 const jumpButton = document.querySelector(".jumpButton");
-const dropdown = document.querySelector("#jumpDiv");
 
 jumpButton.addEventListener("click", e => {
     dropdown.classList.toggle("hide");
 });
-
